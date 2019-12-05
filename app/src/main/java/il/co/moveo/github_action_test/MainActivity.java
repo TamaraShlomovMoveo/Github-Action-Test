b82fc149e4236b41acf5aaac79b1073cd478627a
TamaraShlomovMoveo <tamara@moveo.co.il>
2019-12-05 17:16:05 +0200

    start

---
+++ app/src/main/java/il/co/moveo/github_action_test/MainActivity.java
@@ -0,0 +1,14 @@
+package il.co.moveo.github_action_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

c8c4e911276c641e6e6a9d4c3f01aaad6126142d
TamaraShlomovMoveo <tamara@moveo.co.il>
2019-12-18 10:17:30 +0200

    yallaaaa

--- app/src/main/java/il/co/moveo/github_action_test/MainActivity.java
+++ app/src/main/java/il/co/moveo/github_action_test/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "toast", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "upload to google play store by github-action", Toast.LENGTH_SHORT).show();
     }
 }

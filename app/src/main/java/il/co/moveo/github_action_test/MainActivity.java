1b6d1c5550648bd62a41f2b01b6f0240fb04b131
Ofer Dan-On <ofer@moveo.co.il>
2019-12-26 16:31:56 +0200

    Dummy change

--- app/src/main/java/il/co/moveo/github_action_test/MainActivity.java
+++ app/src/main/java/il/co/moveo/github_action_test/MainActivity.java
@@ -11,6 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        
         Toast.makeText(this, "upload to google play store by github-action", Toast.LENGTH_SHORT).show();
     }
 }

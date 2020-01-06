84c7ceab070a30eae48dd7c987e01df07324b601
Ofer Dan-On <ofer@moveo.co.il>
2020-01-06 11:20:09 +0200

    n

---
+++ .github/actions/upload-apk/main.js
@@ -0,0 +1,28 @@
+const { Apkup } = require('apkup');
+
+const serviceAccount = process.env.SERVICE_ACCOUNT_JSON;
+console.log(serviceAccount);
+console.log(Object.keys(serviceAccount));
+
+const apkup = new Apkup({
+    client_email: 'tamara@moveo.co.il',
+    private_key: serviceAccount.private_key
+});
+
+const releaseFilePath = process.env.GITHUB_WORKSPACE + '/app-release.apk';
+
+apkup
+    .upload(releaseFilePath, {
+        releaseNotes: [
+            {
+                language: 'en-US',
+                text: "DEMO"
+            }
+        ]
+    })
+    .then(data => {
+        console.log('YAY i uploaded successfully');
+    })
+    .catch(err => {
+        console.log("Failed uoloading", err);
+    })

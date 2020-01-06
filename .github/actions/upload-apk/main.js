ea3d524dd1cf7f2a6d2cf7948f8d0285e329c0e8
Ofer Dan-On <ofer@moveo.co.il>
2020-01-06 11:42:38 +0200

    key

--- .github/actions/upload-apk/main.js
+++ .github/actions/upload-apk/main.js
@@ -7,8 +7,9 @@
    console.log(Object.keys(serviceAccount));
 
    const apkup = new Apkup({
-       client_email: 'tamara@moveo.co.il',
-       private_key: serviceAccount.private_key
+       client_email: 'moveo-service-account-212@api-5645872680891446472-637751.iam.gserviceaccount.com',
+//       private_key: serviceAccount.private_key
+       private_key: '58a978016bc8629a7d4fa9d109665d4f3c121086'
    });
 
    const releaseFilePath = process.env.GITHUB_WORKSPACE + '/app-release.apk';
